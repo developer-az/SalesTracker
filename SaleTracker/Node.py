79b2e047a5678b264303d055d22021efdbdc945c
AndyDing28 <dingandy28@gmail.com>
2024-01-11 16:32:23 -0800

    Moved all my work to a git repo

---
+++ SaleTracker/Node.py
@@ -0,0 +1,96 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from apscheduler.schedulers.background import BackgroundScheduler
+import os
+import requests
+from bs4 import BeautifulSoup
+
+os.environ['REQUESTS_CA_BUNDLE'] = '/private/etc/ssl/cert.pem'
+
+app = Flask(__name__)
+CORS(app)
+
+# Initialize the scheduler
+scheduler = BackgroundScheduler(daemon=True)
+scheduler.start()
+
+# Example URL of the product
+product_url = 'https://shop.lululemon.com/p/mens-jackets-and-outerwear/Down-For-It-All-Hoodie/_/prod9200786?color=0001'  # Replace with the actual product URL
+
+# Define a dictionary to store product details
+product_details = {}
+
+# Function to get product details
+def get_product_details():
+    try:
+        # Send a GET request to the URL
+        response = requests.get(product_url)
+        
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # Identify the HTML elements containing the name and price information
+        # These will vary depending on the structure of the website
+        # name_element = soup.find('h1', class_='product-title')  # Update with the correct tag and class
+        name_element = soup.find('h1', class_='product-title_title__i8NUw').find('div')  # Update with the correct tag and class
+        price_element = soup.find('span', class_='price')  # Update with the correct tag and class
+        
+        product_name = name_element.get_text().strip() if name_element else 'Product name not found'
+        product_price = price_element.get_text().strip() if price_element else 'Price not found'
+        print(product_name)
+        return product_name, product_price
+    except Exception as e:
+        return f'Error: {str(e)}'
+
+# Function to send daily email
+def send_daily_email(email):
+    sender_email = os.environ.get('SENDER_EMAIL')
+    password = os.environ.get('EMAIL_PASSWORD')
+
+    if not product_details:
+        # Fetch product details only if not already fetched
+        product_details['name'], product_details['price'] = get_product_details()
+
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = email
+    message['Subject'] = 'Your Daily Email Subject'
+
+    body = f'The price of {product_details["name"]} is: {product_details["price"]}'
+    # body = "ewfae"
+    message.attach(MIMEText(body, 'plain'))
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender_email, password)
+        server.sendmail(sender_email, email, message.as_string())
+        server.quit()
+        print(f"Email sent successfully to {email}")
+    except Exception as e:
+        print(f'Error sending email: {str(e)}')
+
+# Function to schedule the email sending task
+def schedule_email_sending(email):
+    # Schedule the email sending task every day
+    scheduler.add_job(send_daily_email, 'cron', hour=17, minute=00, args=[email])
+
+# Homepage route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route to handle sending email manually (for testing)
+@app.route('/send-email', methods=['POST'])
+def send_email():
+    email = request.json.get('email')
+    send_daily_email(email)    # only for testing, remove this line later
+    schedule_email_sending(email)
+    return jsonify({'message': 'Email sent manually'}), 200
+
+# Start the scheduling when the Flask app is launched
+if __name__ == '__main__':
+    app.run(debug=True)
