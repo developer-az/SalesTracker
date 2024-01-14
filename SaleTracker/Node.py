71100d9a47b13d21b1f701f450369d713b985a5c
AndyDing28 <dingandy28@gmail.com>
2024-01-13 17:17:02 -0800

    testing with local port

--- SaleTracker/Node.py
+++ SaleTracker/Node.py
@@ -7,7 +7,9 @@ from apscheduler.schedulers.background import BackgroundScheduler
 import os
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
