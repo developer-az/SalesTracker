03941e3dca8c35e628743a630d524ab9f0f32fba
AndyDing28 <dingandy28@gmail.com>
2024-02-17 14:31:08 -0500

    merged two files

--- Node.py
+++ Node.py
@@ -4,9 +4,11 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from get_product_details import get_product_details
+from bs4 import BeautifulSoup
 #from dotenv import load_dotenv #remove for deployment onto heroku
 import os
 import smtplib
+import requests
 
 #load_dotenv() #remove for deployment onto heroku
 app = Flask(__name__)
@@ -15,6 +17,16 @@ CORS(app)
 scheduler = BlockingScheduler()
 
 product_url = 'https://shop.lululemon.com/p/mens-jackets-and-outerwear/Down-For-It-All-Hoodie/_/prod9200786?color=0001'
+
+def get_product_details(product_url):
+    response = requests.get(product_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    name_element = soup.find('h1', class_='product-title_title__i8NUw').find('div')
+    price_element = soup.find('span', class_='price')
+        
+    product_name = name_element.get_text().strip() if name_element else 'Product name not found'
+    product_price = price_element.get_text().strip() if price_element else 'Price not found'
+    return product_name, product_price
 
 def send_product_details_email(email):
 
